09617a30ca50cc96275204268662b41904f3db6c
name <name@gmail.com>
2019-05-21 10:28:47 +0900

    説明の追加

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -1,16 +1,15 @@
 <html>
 <head>
   <title>@yield('title')</title>
+  <!-- <table>で(align)等が反応しないためそれぞれで記載すること -->
   <style>
-  /*
-    <table>で(align)等が反応しないためそれぞれで記載すること
-  */
   tr th{text-align: center;}
   tr td{text-align: center;}
   .pagination {font-size: 20pt;}
   .pagination li {display: inline-block;}
   </style>
   <link rel="stylesheet" type="text/css" href="/css/app.css">
+  <!-- 文字化けをさせないようにする -->
   <meta charset="utf-8">
 </head>
 <body>
